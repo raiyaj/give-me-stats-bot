a7f4c57aee0be6c623b9aa36503428f6a20847ca
raiyaj <raiyajessa@gmail.com>
2017-12-18 10:11:53 -0800

    check for #subscribe by searching tweet's hashtag object

--- index.js
+++ index.js
@@ -6,17 +6,28 @@ var config = require('./config.js');
 var T = new Twit(config);
 
 // initialize a user stream (eg. for all activities related to my account)
-var stream = T.stream('user');
+// set tweet mode so that the entities object is populated
+var stream = T.stream('user', { tweet_mode: 'extended' });
 
 // listen for new tweets
 stream.on('tweet', function(tweet) {
   console.log(tweet);
 
-  // filter stream by tweets to me, containing '#subscribe'
-  if (tweet.in_reply_to_screen_name == 'give_me_stats' && tweet.text.indexOf('#subscribe') !== -1) {
+  // check if tweet uses the #subscribe hashtag
+  var tags = Object.keys(tweet.entities.hashtags);
+  var subscribes = false;
+  for (var i = 0; i < tags.length; i++) {
+    if (tweet.entities.hashtags[tags[i]].text.toLowerCase() == 'subscribe') {
+      subscribes = true;
+    }
+  }
+
+  // filter stream by tweets to me, from users requesting to subscribe
+  if (tweet.in_reply_to_screen_name == 'give_me_stats' && subscribes) {
     // tweet a thank-you reply
-    T.post('statuses/update', { status: '@' + tweet.user.screen_name + ' Thank you for subscribing!', in_reply_to_status_id: tweet.id_str },
-    function(err, data, response) {
-    })
+    T.post('statuses/update', { status: '@' + tweet.user.screen_name + ' Thank you for subscribing!',
+    in_reply_to_status_id: tweet.id_str }, function(err, data, response) {
+      console.log(err);
+    });
   }
 });
