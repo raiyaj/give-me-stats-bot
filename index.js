33fd907410b04a499c67e0721dd7ee08a90366bb
raiyaj <raiyajessa@gmail.com>
2017-12-18 02:08:06 -0800

    set up user stream and tweet thank-you for subscribing

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ var Twit = require('twit');  // import the twit package
 // (this will be our connection to the Twitter api via the twit package)
 var config = require('./config.js');
 var T = new Twit(config);
+
+// initialize a user stream (eg. for all activities related to my account)
+var stream = T.stream('user');
+
+// listen for new tweets
+stream.on('tweet', function(tweet) {
+  console.log(tweet);
+
+  // filter stream by tweets to me, containing '#subscribe'
+  if (tweet.in_reply_to_screen_name == 'give_me_stats' && tweet.text.indexOf('#subscribe') !== -1) {
+    // tweet a thank-you reply
+    T.post('statuses/update', { status: '@' + tweet.user.screen_name + ' Thank you for subscribing!', in_reply_to_status_id: tweet.id_str },
+    function(err, data, response) {
+    })
+  }
+});
